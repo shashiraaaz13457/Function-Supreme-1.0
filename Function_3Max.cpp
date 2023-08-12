872934ae502f0b3bce768c062f8e3618885ec156
Shashi raj <shashiraj13457@gmail.com>
2023-08-12 23:50:38 +0530

    Function Practice

---
+++ Function_3Max.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int findMax(int num1, int num2, int num3){
+    if (num1 > num2 && num1 >num3)
+    {
+        cout<<
+    }
+    
+}
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    int maximumNumber = findMax(a,b,c);
+    cout<< maxNumber << endl;
+
+}
